fa6a7a098076ca2cc4fff52b98ed9a7c8d62396c
Polaris106 <wangxingrui2134@gmail.com>
2024-04-14 22:22:01 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, collect_list, size, explode, split, from_json
+from pyspark.sql.functions import col, collect_list, size, explode, split
 from itertools import combinations
 
 # Don't change this line
@@ -14,19 +14,26 @@ input_path = "hdfs://{}:9000/assignment2/part2/input/tmdb_5000_credits.parquet".
 output_path = "hdfs://{}:9000/assignment2/output/question5".format(
     hdfs_nn)
 
-df = spark.read.option("header", "true").parquet(input_path)
+df = spark.read.parquet(input_path)
 
-# Extract pairs of actors/actresses for each movie
-actor_pairs_df = df.select("movie_id", "title", explode(split("cast", ",")).alias("actor")).alias("actor").join(
-    df.select("movie_id", explode(split("cast", ",")
-                                  ).alias("actor")).alias("other_actor"),
-    (col("actor.movie_id") == col("other_actor.movie_id")) & (
-        col("actor.actor") < col("other_actor.actor"))
+# Extract actor/actress pairs for each movie
+actor_pairs_df = df.select(
+    col("id").alias("movie_id"),
+    col("title"),
+    explode(split(col("cast"), ",\s*")).alias("actor1")
+).join(
+    df.select(
+        col("id").alias("movie_id"),
+        explode(split(col("cast"), ",\s*")).alias("actor2")
+    ),
+    "movie_id"
+).filter(
+    col("actor1") < col("actor2")
 ).select(
-    col("actor.movie_id").alias("movie_id"),
+    "movie_id",
     "title",
-    col("actor.actor").alias("actor1"),
-    col("other_actor.actor").alias("actor2")
+    col("actor1").alias("actor1"),
+    col("actor2").alias("actor2")
 )
 
 # Group by actor pairs and count the number of movies they co-cast in
@@ -37,9 +44,12 @@ co_cast_df = actor_pairs_df.groupBy("actor1", "actor2").agg(
 # Explode the list of movie IDs and select required columns
 result_df = co_cast_df.select(
     col("movie_ids").getItem(0).alias("movie_id"),
+    "title",
     "actor1",
     "actor2"
 )
 
 # Save the result into Parquet files with the specified schema
-result_df.write.parquet(output_path)
+result_df.write.mode("overwrite").parquet(output_path)
+
+spark.stop()
