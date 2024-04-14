292a3112846b567a626c8e37513969ed8033ca17
Polaris106 <wangxingrui2134@gmail.com>
2024-04-14 21:45:58 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -11,7 +11,7 @@ spark = SparkSession.builder.appName("Assignment 2 Question 5").getOrCreate()
 # Read movie data from Parquet file
 input_path = "hdfs://{}:9000/assignment2/part2/input/tmdb_5000_credits.parquet".format(
     hdfs_nn)
-output_path = "hdfs://{}:9000/assignment2/part2/input/tmdb_5000_credits.parquet".format(
+output_path = "hdfs://{}:9000/assignment2/output/question5".format(
     hdfs_nn)
 
 df = spark.read.option("header", "true").parquet(input_path)
