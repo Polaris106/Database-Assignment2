afdfbbf8b469dca321b0c46ac4d1f32a4b5a62d4
Polaris106 <wangxingrui2134@gmail.com>
2024-04-14 15:33:37 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,59 +1,39 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, max, min
-
-# you may add more import if you need to
-
-
+from pyspark.sql.functions import col, max as max_, min as min_, row_number
+from pyspark.sql.window import Window
 # don't change this line
 hdfs_nn = sys.argv[1]
 
-spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
-# YOUR CODE GOES BELOW
+spark = SparkSession.builder.appName("Assignment 2 Question 2").getOrCreate()
 
-df = (
-    spark.read.option("header", True)
-    .option("inferSchema", True)
-    .option("delimiter", ",")
-    .option("quotes", '"')
-    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
-)
+input_file_name = 'hdfs://%s:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv' % (
+    hdfs_nn)
+output_dir_name = 'hdfs://%s:9000/assignment2/output/question2' % (hdfs_nn)
 
-# Drop Price Range  Rating with null
-df = df.dropna(how="any", subset=["Price Range", "Rating"])
+df = spark.read.csv(input_file_name, header=True)
+df = df.filter(df["Price Range"].isNotNull())
 
-# Max rating in each city
-max_df = (
-    df.groupBy(["Price Range", "City"])
-    .agg(max("Rating"))
-    .withColumn("Rating", col("max(Rating)"))
-    .drop("max(Rating)")
-)
+df = df.withColumn("Rating", df["Rating"].cast("double"))
 
-min_df = (
-    df.groupBy(["Price Range", "City"])
-    .agg(min("Rating"))
-    .withColumn("Rating", col("min(Rating)"))
-    .drop("min(Rating)")
-)
+grouped_df = df.groupBy("City", "Price Range").agg(
+    max_("Rating").alias("Max Rating"), min_("Rating").alias("Min Rating"))
 
-new_df = max_df.union(min_df).join(df, on=["Price Range", "City", "Rating"], how="inner")
+# row numbers partitioned by City and Price Range, ordered by Rating
+window_best = Window.partitionBy(
+    "City", "Price Range").orderBy(col("Rating").desc())
+window_worst = Window.partitionBy(
+    "City", "Price Range").orderBy(col("Rating").asc())
 
-new_df = (new_df.dropDuplicates(["Price Range", "City", "Rating"])
-          .select(
-            "_c0",
-            "Name",
-            "City",
-            "Cuisine Style",
-            "Ranking",
-            "Rating",
-            "Price Range",
-            "Number of Reviews",
-            "Reviews",
-            "URL_TA",
-            "ID_TA",
-          )
-          .sort(col("City").asc(), col("Price Range").asc(), col("Rating").desc())
-        )
+# top-ranked and lowest-ranked restaurants bc there are multiple 5 star and 1 star for each city/price range
+best_restaurants = df.withColumn("rank", row_number().over(
+    window_best)).filter(col("rank") == 1).drop("rank")
+worst_restaurants = df.withColumn("rank", row_number().over(
+    window_worst)).filter(col("rank") == 1).drop("rank")
 
-new_df.write.csv("hdfs://%s:9000/assignment2/output/question2/" % (hdfs_nn), header=True)
+# best + worst
+result_df = best_restaurants.union(worst_restaurants).orderBy("City")
+result_df.show()
+# result_df.show()
+result_df.write.mode('overwrite').option("header", True).csv(output_dir_name)
+spark.stop()
